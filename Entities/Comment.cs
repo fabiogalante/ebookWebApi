091899bb11f9a1d804a3d5a77c6b194721a278a2
Fabio <fabio@MacBook-Pro-de-Fabio.local>
2018-08-21 22:50:37 -0300

    Entities

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -1,7 +1,12 @@
-namespace ebookWebApi.Entities
-{
-    public class Comment
-    {
-        
+using System;
+
+namespace ebookWebApi.Entities {
+    public class Comment {
+        public int CommentId { get; set; }
+        public int Rating { get; set; }
+        public string DishComment { get; set; }
+        public DateTime date { get; set; }
+        public CrUser User { get; set; }
+        public Dish Dish { get; set; }
     }
 }
--- Entities/Dish.cs
+++ Entities/Dish.cs
@@ -1,7 +1,11 @@
-namespace ebookWebApi.Entities
-{
-    public class Dish
-    {
-        
+namespace ebookWebApi.Entities {
+    public class Dish {
+        public int DishId { get; set; }
+        public string DishName { get; set; }
+        public string DishLabel { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
